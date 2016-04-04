f1da406b6aba5a7131666056108a3d3b9a888395
Brad Pizzimenti and Kyle Derrick <kderrick1988@gmail.com>
2016-04-04 10:22:44 -0700

    store and display timestamp for of ticket creation

--- app/components/new-ticket.js
+++ app/components/new-ticket.js
@@ -6,7 +6,8 @@ export default Ember.Component.extend({
       var params = {
         pairname: this.get('pairname'),
         location: this.get('location'),
-        comments: this.get('comments')
+        comments: this.get('comments'),
+        timestamp: new Date()
       };
       this.set('comments', "");
       this.sendAction('save', params);
--- app/models/ticket.js
+++ app/models/ticket.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   pairname: DS.attr(),
   location: DS.attr(),
-  comments: DS.attr()
+  comments: DS.attr(),
+  timestamp: DS.attr()
 });
