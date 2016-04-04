9276f5a737685598bbe4739ce9e3e0977e01c341
Brad Pizzimenti and Kyle Derrick <brad@gennakersystems.com>
2016-04-04 09:42:59 -0700

    create new tickets

--- app/components/new-ticket.js
+++ app/components/new-ticket.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        pairname: this.get('pairname'),
+        location: this.get('location'),
+        comments: this.get('comments')
+      };
+      this.sendAction('save', params);
+    }
+  }
 });
