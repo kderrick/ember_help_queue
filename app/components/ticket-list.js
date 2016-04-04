01910d48674d6c83fa276b79e884df970354f580
Brad Pizzimenti and Kyle Derrick <kderrick1988@gmail.com>
2016-04-04 10:50:52 -0700

    display waitTime as a computed property

--- app/components/ticket-list.js
+++ app/components/ticket-list.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  waitTime: Ember.computed('ticket.timestamp', function() {
+    var currentTime = new Date();
+    var ticketTime = new Date(this.get('ticket.timestamp')) ;
+    var waitTime = currentTime - ticketTime;
+    console.log("Current Time:", currentTime);
+    console.log("Ticket Time:", ticketTime);
+    console.log("Computed Wait:", waitTime/1000);
+    if(waitTime/60000/60/24 > 1) {
+      return "Invalid Datestamp, please resubmit"
+    } 
+    return parseInt(waitTime/60000);
+  }),
+
   actions: {
     delete(ticket) {
       if(confirm("Do you want to delete this ticket?")) {
